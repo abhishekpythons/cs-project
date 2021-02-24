20bcd8b8d7546f967ba82a0bbdbda131179744c3
unknown <abhishekpythons@gmail.com>
2021-02-25 05:00:06 +0530

    email content extended

--- confess/views.py
+++ confess/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import smtplib
+from email.message import EmailMessage
 from . import config
 
 def view_form(request):
     return render(request, 'login.html')
 
 def send_email(From_name, To_email, msg):
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.ehlo()
-        server.starttls()
-        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
-        message = f'From: {From_name} \n {msg}'
-        server.sendmail(config.EMAIL_ADDRESS, To_email, message)
-        server.quit()
+        message = EmailMessage()
+        message['Subject'] = f'confession from: {From_name}'
+        message['From'] = config.EMAIL_ADDRESS
+        message['To'] = To_email
+        msg = str( msg + '\n'*10 + 'This is a mail send via confessinator.herokuapp.com \n here name(%s) shown may be real or fake.\nBut intentions and emotions are true. \nOur Team is doing work of filling gaps.\nRegards confessinator team :)'%From_name)
+        message.set_content(msg)
+        with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+            smtp.login(config.EMAIL_ADDRESS, config.PASSWORD)
+            smtp.send_message(message)
+
+
 
 @csrf_exempt
 def read_form(request):
